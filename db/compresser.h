73c8098c8cfec3a8febd428b3cf2597127a5cfd5
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-03-26 14:43:22 +0800

    add a operator to compresser

--- db/compresser.h
+++ db/compresser.h
@@ -11,12 +11,13 @@ class compresser
 public:
     virtual ~compresser() noexcept {}
     virtual ::std::string compress(::std::string str) = 0;
+    auto operator()(auto&& str) { return compress(::std::forward<decltype(str)>(str)); }
 };
 
 class nop_compresser : final public compresser
 {
 public:
-    ::std::string compress(::std::string str) noexcept toverride
+    ::std::string compress(::std::string str) noexcept override
     {
         return str;
     }
