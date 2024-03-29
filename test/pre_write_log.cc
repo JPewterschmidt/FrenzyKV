9157dff6ed4a24921a281a6ff6127750b1d6b171
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-03-30 00:39:27 +0800

    add pre_write_log test

---
+++ test/pre_write_log.cc
@@ -0,0 +1,72 @@
+#include "gtest/gtest.h"
+#include "log/logger.h"
+#include "frenzykv/write_batch.h"
+#include "frenzykv/env.h"
+#include "koios/iouring_awaitables.h"
+#include "toolpex/errret_thrower.h"
+#include <string>
+
+using namespace frenzykv;
+using namespace ::std::string_literals;
+
+namespace
+{
+
+write_batch make_batch()
+{
+    write_batch result;
+    result.write("xxxx", "abc");
+    result.write("aaaa", "def");
+    result.set_first_sequence_num(0);
+
+    return result;
+}
+
+koios::task<bool> write(logger& l)
+{
+    auto w = make_batch();
+    try
+    {
+        co_await l.write(w);
+    }
+    catch (...)
+    {
+        co_return false;
+    }
+    co_return true;
+}
+
+koios::task<toolpex::unique_posix_fd> open_file()
+{
+    toolpex::errret_thrower et;
+    co_return et << ::open("pre_write_test.txt", O_RDWR);
+}
+
+koios::eager_task<bool> read()
+{
+    auto fd = co_await open_file();
+    ::std::array<char, 1024> buffer{};
+    co_await koios::uring::read(fd, ::std::as_writable_bytes(::std::span{buffer}));
+    entry_pbrep e;
+    e.ParseFromArray(buffer.data(), buffer.size());
+    co_return e.key().user_key() == "aaaa"s;
+}
+
+koios::eager_task<> clean()
+{
+    co_await koios::uring::unlink("pre_write_test.txt");
+}
+
+} // annoymous namespace
+
+TEST(logger, basic)
+{
+    auto opt = get_global_options();
+    auto e = env::make_default_env(opt);
+    opt.environment = e.get();
+    logger l(opt, "pre_write_test.txt");
+    ASSERT_TRUE(write(l).result());
+    ASSERT_TRUE(read().result());
+
+    clean().result();
+}
