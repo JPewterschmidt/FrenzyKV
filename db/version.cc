0dbfcd490c1226c88eecd93d8fd68a259f7a969b
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-04 15:55:55 +0800

    add version_impl_simple

--- db/version.cc
+++ db/version.cc
@@ -1,34 +1,62 @@
 #include "db/version.h"
+#include "koios/coroutine_shared_mutex.h"
+
+namespace frenzykv::version_detials
+{
+
+class version_impl_simple : public abstract_versions
+{
+public:
+    virtual koios::task<sequence_number_t> 
+    set_last_sequence_number(sequence_number_t newver) override
+    {
+        auto lk = co_await m_mut.acquire();
+        co_return (m_last_seq = newver);
+    }
+
+    virtual koios::task<sequence_number_t> 
+    last_sequence_number() const override
+    { 
+        auto lk = co_await m_mut.acquire_shared();
+        co_return m_last_seq;
+    }
+
+private:
+    mutable koios::shared_mutex m_mut;
+    sequence_number_t m_last_seq{};
+};
+
+::std::unique_ptr<abstract_versions> 
+default_instance(::std::string filename)
+{
+    using namespace version_detials;
+    return ::std::make_unique<version_impl_simple>();
+}
+
+} // namespace frenzykv::version_detials
 
 namespace frenzykv
 {
 
 version_hub::version_hub(const options& opt)
-    : m_opt{ &opt } // TODO pimpl
+    : m_opt{ &opt }, 
+      m_pimpl{ version_detials::default_instance() }
 {
+    assert(m_pimpl != nullptr);
 }
 
-void 
+koios::task<> 
 version_hub::
-commit(version_record newver)
+set_last_sequence_number(sequence_number_t newver)
 {
-    assert(m_pimpl);
-    m_pimpl->commit(::std::move(newver));
+    co_await m_pimpl->set_last_sequence_number(::std::move(newver));
 }
 
-version_record 
-version_hub::
-current() const
-{
-    assert(m_pimpl);
-    return m_pimpl->current();
-}
-
-sequence_number_t 
+koios::task<sequence_number_t>
 version_hub::
 last_sequence_number() const 
 { 
-    return m_pimpl->last_sequence_number(); 
+    co_return co_await m_pimpl->last_sequence_number(); 
 }
 
 } // namespace frenzykv
--- db/version.h
+++ db/version.h
@@ -4,38 +4,41 @@
 #include <memory>
 #include <chrono>
 #include "frenzykv/options.h"
+#include "toolpex/move_only.h"
 
 namespace frenzykv
 {
 
 using sequence_number_t = uint64_t;
 
-struct version_record
-{
-    sequence_number_t m_sequence_number{};
-    ::std::chrono::system_clock::time_point m_timestemp{ ::std::chrono::system_clock::now() };
-};
-
 class abstract_versions
 {
 public:
-    virtual version_record current() const = 0;
-    virtual void commit(version_record newver) = 0;
-    virtual sequence_number_t last_sequence_number() const { return current().m_sequence_number; }
+    virtual koios::task<sequence_number_t> 
+    set_last_sequence_number(sequence_number_t newver) = 0;
+
+    virtual koios::task<sequence_number_t> 
+    last_sequence_number() const = 0;
 };
 
-class version_hub
+namespace version_detials{ 
+
+::std::unique_ptr<abstract_versions> 
+default_instance(::std::string filename = "");
+
+} // namespace version_detials
+
+class version_hub : public toolpex::move_only
 {
 public:
     version_hub(const options& opt);
 
-    void commit(version_record newver);
-    version_record current() const;
-    sequence_number_t last_sequence_number() const;
+    koios::task<>                   set_last_sequence_number(sequence_number_t newver);
+    koios::task<sequence_number_t>  last_sequence_number() const;
 
 private:
     const options* m_opt;
-    const ::std::unique_ptr<abstract_versions> m_pimpl;
+    ::std::unique_ptr<abstract_versions> m_pimpl = version_detials::default_instance();
 };
 
 } // namespace frenzykv
