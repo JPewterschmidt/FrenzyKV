4c70f4571cae3cdd3667dc58d2895581b79e6af1
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-14 22:45:26 +0800

    do some tests

--- example/main.cc
+++ example/main.cc
@@ -20,47 +20,11 @@ using namespace koios;
 using namespace frenzykv;
 using namespace ::std::string_view_literals;
 
-task<> ostest()
-try
+koios::task<> file_test
 {
-    seq_key key1, key2, key3, key4;
-    ::std::string key1_str, key2_str, key3_str, key4_str;
-
-    key1.set_user_key("xxxxxxxxx");
-    key1.set_seq_number(0);
-
-    key2.set_user_key("xxxxxxxxx");
-    key2.set_seq_number(1);
-
-    key3.set_user_key("xxxxxxxxx");
-    key3.set_seq_number(10000000);
-
-    key4.set_user_key("xxxxxxxxx");
-    key4.set_seq_number(100000000);
-
-    key1.SerializeToString(&key1_str);
-    key2.SerializeToString(&key2_str);
-    key3.SerializeToString(&key3_str);
-    key4.SerializeToString(&key4_str);
-
-    ::std::cout << ::std::less{}(key1_str, key2_str) << ::std::endl;
-    ::std::cout << ::std::less{}(key1_str, key3_str) << ::std::endl;
-    ::std::cout << ::std::less{}(key3_str, key4_str) << ::std::endl;
-
-    co_return;
-}
-catch (const koios::exception& e)
-{
-    e.log();
 }
 
 int main()
 {
-    size_t hash_result{};
-    seq_key k;
-    k.set_user_key("xxxxxxxxx");
-    k.set_seq_number(0);
 
-    ::std::array<char, 128> buffer{};
-    k.SerializeToArray(buffer.data(), buffer.size());
 }
