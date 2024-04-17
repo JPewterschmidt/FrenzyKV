62e5081965e6412a8282cbeb8aa14adb900a0a46
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-17 22:20:40 +0800

    add parse_result enum

---
+++ include/frenzykv/util/parse_result.h
@@ -0,0 +1,16 @@
+#ifndef FRENZYKV_PARSE_RESULT_H
+#define FRENZYKV_PARSE_RESULT_H
+
+namespace frenzykv
+{
+
+enum class parse_result_t
+{
+    success, 
+    partial, 
+    error,
+};
+
+} // namespace frenzykv
+
+#endif
