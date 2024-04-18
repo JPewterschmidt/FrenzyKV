d28012f714bc8229ecd2a0a69a54a962344a99b6
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-18 22:19:12 +0800

    add db destroy

---
+++ include/frenzykv/destroy_db.h
@@ -0,0 +1,13 @@
+#ifndef FRENZYKV_DESTROY_DB_H
+#define FRENZYKV_DESTROY_DB_H
+
+#include <filesystem>
+
+namespace frenzykv 
+{
+
+void destroy_db(::std::filesystem::path p);
+
+} // namespace frenzykv
+
+#endif
