c3b0ee28660d916cb014c126803c58b5ba7beb8a
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-19 20:58:27 +0800

    add compressor

---
+++ util/compressor.cc
@@ -0,0 +1,58 @@
+#include "frenzykv/util/compressor.h"
+#include "zstd.h"
+
+namespace frenzykv
+{
+
+class zstd_compressor final : public compressor_policy
+{
+public:
+    ::std::string_view name() const noexcept override { return "zstd_compressor"; }
+
+    ::std::error_code 
+    compress(const_bspan original, 
+             ::std::string& compressed_dst) const override
+    {
+        compressed_dst.clear();
+        compressed_dst.resize(::ZSTD_compressBound(original.size()), 0);
+
+        const size_t sz_compressed = ::ZSTD_compress(
+            compressed_dst.data(), compressed_dst.size(), 
+            original.data(), original.size(), 
+            ::ZSTD_maxCLevel()
+        );
+
+        if (::ZSTD_isError(sz_compressed))
+        {
+            // TODO return a ec with ZSTD category
+            return {};
+        }
+        compressed_dst.resize(sz_compressed);
+
+        return {};
+    }
+
+    ::std::error_code 
+    decompress(const_bspan compressed_src, 
+               ::std::string& decompressed_dst) const override
+    {
+        decompressed_dst.clear();
+        decompressed_dst.resize(::ZSTD_getFrameContentSize(compressed_src.data(), compressed_src.size()));
+
+        const size_t sz_decompr = ::ZSTD_decompress(
+            decompressed_dst.data(), decompressed_dst.size(), 
+            compressed_src.data(), compressed_src.size()
+        );
+
+        if (::ZSTD_isError(sz_decompr))
+        {
+            // TODO return a ec with ZSTD category
+            return {};
+        }
+        decompressed_dst.resize(sz_decompr);
+
+        return {};
+    }
+};
+
+} // namespace frenzykv
