cfa35d60251b22d28346aec50d7953a01ae32ad5
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-21 00:17:36 +0800

    complete `sstable::parse_meta_data()`

--- persistent/sstable.cc
+++ persistent/sstable.cc
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "toolpex/exceptions.h"
 #include "frenzykv/persistent/sstable.h"
 #include "frenzykv/persistent/sstable_builder.h"
@@ -8,8 +10,9 @@ namespace frenzykv
 koios::task<bool> sstable::parse_meta_data()
 {
     const uintmax_t filesz = m_file->file_size();
-    ::std::array<::std::byte, sizeof(mbo_t) + sizeof(mgn_t)> buffer{};
-    co_await m_file->read(buffer, filesz);
+    ::std::string buffer(sizeof(mbo_t) + sizeof(mgn_t), 0);
+    
+    co_await m_file->read({ buffer.data(), buffer.size() }, filesz);
     mbo_t mbo{};
     mgn_t magic_num{};
     ::std::memcpy(&mbo, buffer.data(), sizeof(mbo));
@@ -20,11 +23,24 @@ koios::task<bool> sstable::parse_meta_data()
     {
         co_return false;
     }
-    
-    // TODO
-    
-    
-    co_return {};
+
+    // For meta Block
+    buffer = ::std::string(filesz - mbo - sizeof(mbo_t) - sizeof(mgn_t), 0);
+    co_await m_file->read({ buffer.data(), buffer.size() }, mbo);
+    block meta_block{ ::std::as_bytes(::std::span{buffer}) };
+    assert(meta_block.special_segments_count() == 1);
+    for (block_segment seg : meta_block.segments_in_single_interval())
+    {
+        if (as_string_view(seg.public_prefix()) == "bloom_filter")
+        {
+            auto fake_user_value_sp = seg.items().front();
+            auto uv = kv_user_value::parse(fake_user_value_sp);
+            m_filter_rep = uv.value();
+            break; // Until now, there should be only one segment, only one element in segment.
+        }
+    }
+
+    co_return true;
 }
 
 } // namespace frenzykv
