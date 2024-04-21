b164094dbfa92de52371a270af2e484e28412187
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-22 01:54:20 +0800

    add a comment to notify my self

--- include/frenzykv/util/serialize_helper.h
+++ include/frenzykv/util/serialize_helper.h
@@ -14,6 +14,7 @@ namespace frenzykv
 template<::std::size_t intsize>
 void append_encode_int_to(::std::integral auto i, ::std::string& dst)
 {
+    // TODO: add endian check and byteswap
     static ::std::array<char, intsize> buffer{};
     static_assert(sizeof(i) == intsize, "privided size and actual size mismatch in Serializer_helper.h: append_encode_int_to() function ");
     ::std::memcpy(buffer.data(), &i, sizeof(i));
@@ -23,6 +24,7 @@ void append_encode_int_to(::std::integral auto i, ::std::string& dst)
 template<::std::size_t intsize>
 void encode_int_to(::std::integral auto i, bspan buffer)
 {
+    // TODO: add endian check and byteswap
     assert(buffer.size_bytes() == intsize);
     ::std::memcpy(buffer.data(), &i, intsize);
 }
@@ -30,6 +32,7 @@ void encode_int_to(::std::integral auto i, bspan buffer)
 template<::std::size_t intsize>
 void encode_int_to(::std::integral auto i, ::std::span<char> buffer)
 {
+    // TODO: add endian check and byteswap
     encode_int_to<intsize>(i, ::std::as_writable_bytes(buffer));
 }
 
