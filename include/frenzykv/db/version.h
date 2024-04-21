848d8f889e54549dcb964e96fd8c51cecd956778
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-21 22:21:22 +0800

    redesign version

--- include/frenzykv/db/version.h
+++ include/frenzykv/db/version.h
@@ -1,8 +1,35 @@
 #ifndef FRENZYKV_DB_VERSION_H
 #define FRENZYKV_DB_VERSION_H
 
+#include <memory>
+
+#include "toolpex/ref_count.h"
+#include "koios/task.h"
+
 namespace frenzykv
 {
+
+class version_delta
+{
+public:
+private:
+};
+
+class version
+{
+public:
+    using sptr = ::std::shared_ptr<version>;
+    
+public:
+    sequence_number_t sequence_number() const noexcept { return m_seq; }
+    ~version() noexcept;
+
+    bool    operator<(const version& other) const noexcept;
+    version operator+(const version_delta& delta) const noexcept;
+
+private:
+    sequence_number_t m_seq;
+};
 
 class version_set
 {
@@ -11,10 +38,9 @@ public:
         : m_deps{ &deps }, m_current{ current };
     {
     }
-
+    
 private:
     const kvdb_deps* m_deps;
-    ::std::set<sequence_number_t> m_currently_using;
     sequence_number_t m_current;
 };
 
