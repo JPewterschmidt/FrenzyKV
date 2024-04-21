b96538b1924d78771424b6fbca6458858f9d8209
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-22 02:00:27 +0800

    change the integer encode format to big endian

--- include/frenzykv/util/serialize_helper.h
+++ include/frenzykv/util/serialize_helper.h
@@ -1,6 +1,7 @@
 #ifndef FRENZYKV_SERIALIZER_HELPER_H
 #define FRENZYKV_SERIALIZER_HELPER_H
 
+#include <bit>
 #include <string>
 #include <cstddef>
 #include <cstdint>
@@ -11,12 +12,29 @@
 namespace frenzykv
 {
 
+auto big_endian_encode(::std::integral auto i)
+{
+    if constexpr (::std::endian::native == ::std::endian::big)
+    {
+        return i;
+    }
+    else if constexpr (::std::endian::native == ::std::endian::little)
+    {
+        return ::std::byteswap(i);
+    }
+    else // mixed endian
+    {
+        ::std::terminate();
+        return {};
+    }
+}
+
 template<::std::size_t intsize>
 void append_encode_int_to(::std::integral auto i, ::std::string& dst)
 {
-    // TODO: add endian check and byteswap
     static ::std::array<char, intsize> buffer{};
     static_assert(sizeof(i) == intsize, "privided size and actual size mismatch in Serializer_helper.h: append_encode_int_to() function ");
+    i = big_endian_encode(i);
     ::std::memcpy(buffer.data(), &i, sizeof(i));
     dst.append(buffer.data(), buffer.size());
 }
@@ -24,15 +42,15 @@ void append_encode_int_to(::std::integral auto i, ::std::string& dst)
 template<::std::size_t intsize>
 void encode_int_to(::std::integral auto i, bspan buffer)
 {
-    // TODO: add endian check and byteswap
     assert(buffer.size_bytes() == intsize);
+    i = big_endian_encode(i);
     ::std::memcpy(buffer.data(), &i, intsize);
 }
     
 template<::std::size_t intsize>
 void encode_int_to(::std::integral auto i, ::std::span<char> buffer)
 {
-    // TODO: add endian check and byteswap
+    // forward to another function, must not to byteswap
     encode_int_to<intsize>(i, ::std::as_writable_bytes(buffer));
 }
 
