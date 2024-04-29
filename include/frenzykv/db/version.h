6f9c75f8b84d81452afc9c9eace06575776603b1
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-30 00:42:11 +0800

    implement version_delta

--- include/frenzykv/db/version.h
+++ include/frenzykv/db/version.h
@@ -2,9 +2,13 @@
 #define FRENZYKV_DB_VERSION_H
 
 #include <memory>
+#include <ranges>
+#include <vector>
 
 #include "toolpex/ref_count.h"
 #include "koios/task.h"
+
+#include "frenzykv/types.h"
 
 namespace frenzykv
 {
@@ -12,7 +16,30 @@ namespace frenzykv
 class version_delta
 {
 public:
+    decltype(auto) add_compacted_files(::std::ranges::range auto const& ids)
+    {
+        m_compacted.append_range(ids);
+        return *this;
+    }
+
+    decltype(auto) add_new_files(::std::ranges::range auto const& ids)
+    {
+        m_added.append_range(ids);
+        return *this;
+    }
+
+    decltype(auto) add_new_file(file_id_t id)
+    {
+        m_added.emplace_back(id);
+        return *this;
+    }
+
+    const auto& comapcted_files() const noexcept { return m_compacted; }
+    const auto& added_files() const noexcept { return m_added; }
+
 private:
+    ::std::vector<file_id_t> m_compacted;
+    ::std::vector<file_id_t> m_added;
 };
 
 class version
