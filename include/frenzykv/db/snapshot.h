b05b31db45d46e8e084eafcd840e8036742e3d57
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-05-01 00:56:46 +0800

    remove old snapshot implementation

--- include/frenzykv/db/snapshot.h
+++ include/frenzykv/db/snapshot.h
@@ -4,16 +4,6 @@
 namespace frenzykv
 {
 
-class snapshot
-{
-public:
-    constexpr snapshot() noexcept = default;   
-    sequence_number_t sequence_number() const noexcept { return m_seq; }
-
-private:
-    sequence_number_t m_seq{};
-    // VERSION REF
-};
 
 } // namesapce frenzykv
 
