594c0081076ee4882a6adfea9a6d6c705492624a
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-05-05 01:39:30 +0800

    apply `compaction_policy_oldest` to default_compaction_policy

--- persistent/compaction_policy.cc
+++ persistent/compaction_policy.cc
@@ -1,11 +1,13 @@
 #include "frenzykv/persistent/compaction_policy.h"
 
+#include "frenzykv/persistent/compaction_policy_oldest.h"
+
 namespace frenzykv
 {
 
 ::std::unique_ptr<compaction_policy> make_default_compaction_policy()
 {
-    return {};
+    return ::std::make_unique<compaction_policy_oldest>();
 }
 
 } // namespace frenzykv
