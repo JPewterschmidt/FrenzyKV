7f90c37b4c5243c6f0c99ca31ba3a912a1cfbb43
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-05-05 02:44:43 +0800

    update `compaction_policy_oldest`

--- include/frenzykv/persistent/compaction_policy_oldest.h
+++ include/frenzykv/persistent/compaction_policy_oldest.h
@@ -9,8 +9,17 @@ namespace frenzykv
 class compaction_policy_oldest : public compaction_policy
 {
 public:
-    ::std::vector<file_guard> 
+    compaction_policy_oldest(const kvdb_deps& deps, filter_policy* filter) noexcept
+        : m_deps{ &deps }, m_filter{ filter }
+    {
+    }
+
+    koios::task<::std::vector<file_guard>>
     compacting_files(const version_guard& vc, level_t from) const override;
+
+private:
+    const kvdb_deps* m_deps{};
+    filter_policy* m_filter{};
 };
 
 } // namespace frenzykv
--- persistent/compaction_policy_oldest.cc
+++ persistent/compaction_policy_oldest.cc
@@ -4,13 +4,14 @@
 #include "frenzykv/env.h"
 
 #include "frenzykv/persistent/compaction_policy_oldest.h"
+#include "frenzykv/persistent/sstable.h"
 
 namespace fs = ::std::filesystem;
 
 namespace frenzykv
 {
 
-::std::vector<file_guard> 
+koios::task<::std::vector<file_guard>>
 compaction_policy_oldest::
 compacting_files(const version_guard& vc, level_t from) const
 {
@@ -19,8 +20,13 @@ compacting_files(const version_guard& vc, level_t from) const
     const auto& files = vc.files();
     file_guard guard_oldest_file;
     fs::file_time_type ftime = fs::file_time_type::max();
+
+    // Find oldest file from level `from`
     for (const auto& fguard : files)
     {
+        if (fguard.level() != from)
+            continue;
+        
         if (auto lmt = fs::last_write_time(sstables_path()/fguard.name()); 
             lmt < ftime)
         {
@@ -28,9 +34,27 @@ compacting_files(const version_guard& vc, level_t from) const
             guard_oldest_file = fguard;
         }
     }
+    auto fp = result.emplace_back(::std::move(guard_oldest_file)).open_read(m_deps->env().get());
+    sstable from_l_sst(*m_deps, m_filter, fp.get());
+    [[maybe_unused]] bool pr = co_await from_l_sst.parse_meta_data(); assert(pr);
+
+    // Find overlapped tables from next level
+    for (const auto& fguard : files)
+    {
+        if (fguard.level() != from + 1)
+            continue;
+        
+        ::std::unique_ptr<random_readable> fp = fguard.open_read(m_deps->env().get());
+        sstable next_l_sst(*m_deps, m_filter, fp.get());
+        co_await next_l_sst.parse_meta_data();
+
+        if (from_l_sst.overlapped(next_l_sst))
+        {
+            result.push_back(fguard);
+        }
+    }
     
-    result.emplace_back(::std::move(guard_oldest_file));
-    return result;
+    co_return result;
 }
 
 } // namespace frenzykv
