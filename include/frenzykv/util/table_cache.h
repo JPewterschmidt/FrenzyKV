0cfb0a589251c56513ef469008a967a6d4ee9fe3
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-05-14 00:04:20 +0800

    implement table_cache (#2), without using system statistics

--- include/frenzykv/util/table_cache.h
+++ include/frenzykv/util/table_cache.h
@@ -1,6 +1,7 @@
 #ifndef FRENZYKV_UTIL_TABLE_CACHE_H
 #define FRENZYKV_UTIL_TABLE_CACHE_H
 
+#include <memory>
 #include <string>
 #include <string_view>
 
@@ -16,11 +17,15 @@ namespace frenzykv
 class table_cache
 {
 public:
-    koios::task<const sstable*> find_table(::std::string_view name);
-    koios::task<> insert(sstable table);
+    table_cache(size_t capasity);
+
+    koios::task<::std::shared_ptr<sstable>> 
+    find_table(const ::std::string& name);
+
+    koios::task<::std::shared_ptr<sstable>> insert(sstable&& table);
     
 private:
-    toolpex::lru_cache<::std::string, sstable> m_tables;
+    toolpex::lru_cache<::std::string, ::std::shared_ptr<sstable>> m_tables;
     mutable koios::shared_mutex m_update_mutex;
 };
 
--- util/table_cache.cc
+++ util/table_cache.cc
@@ -3,20 +3,28 @@
 namespace frenzykv
 {
 
-koios::task<const sstable*> table_cache::
-find_table(::std::string_view name)
+table_cache::table_cache(size_t capacity)
+    : m_tables{ capacity }
 {
-    auto shr = co_await m_update_mutex.acquire_shared();
-    toolpex::not_implemented();
-    co_return {};
 }
 
-koios::task<> table_cache::
-insert(sstable table)
+koios::task<::std::shared_ptr<sstable>> 
+table_cache::
+find_table(const ::std::string& name)
+{
+    auto shr = co_await m_update_mutex.acquire_shared();
+    auto opt = m_tables.get(name);
+    co_return opt ? ::std::move(*opt) : nullptr;
+}
+
+koios::task<::std::shared_ptr<sstable>> table_cache::
+insert(sstable&& table)
 {
     auto uni = co_await m_update_mutex.acquire();
-    toolpex::not_implemented();
-    co_return;
+    const auto name = table.filename();
+    auto result = ::std::make_shared<sstable>(::std::move(table));
+    m_tables.put(::std::string(name), result);
+    co_return result;   
 }
     
 } // namespace frenzykv
