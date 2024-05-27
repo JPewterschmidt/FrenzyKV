0f8a693516e256303d7c6a43463e9cae762415bd
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-05-27 19:01:05 +0800

    add `sstable_getter_from_file`

---
+++ util/sstable_getter_from_file.cc
@@ -0,0 +1,13 @@
+#include "frenzykv/util/sstable_getter_from_file.h"
+
+namespace frenzykv
+{
+
+koios::task<::std::shared_ptr<sstable>> sstable_getter_from_file::
+get(const file_guard& fg) const 
+{
+    auto env = m_deps->env();
+    co_return ::std::make_shared<sstable>(*m_deps, m_filter, co_await fg.open_read(env.get()));
+}
+
+} // namespace frenzykv
