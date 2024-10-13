ee776fa1712a036b25ea4553c9d288634ea5f349
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-10-13 18:05:09 +0800

    remove unnecessary `env` object

--- table/sstable_getter_from_file.cc
+++ table/sstable_getter_from_file.cc
@@ -12,7 +12,6 @@ namespace frenzykv
 koios::task<::std::shared_ptr<sstable>> sstable_getter_from_file::
 get(file_guard fg) const 
 {
-    auto env = m_deps->env();
     co_return co_await sstable::make(*m_deps, m_filter, co_await fg.open_read());
 }
 
